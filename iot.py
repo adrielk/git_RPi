8a78d181e5b1424d6d7160426b33238bc1bf5622
adrielk <theadrielkim@gmail.com>
2021-01-16 20:12:49 -0500

    image view control

--- iot.py
+++ iot.py
@@ -1,11 +1,59 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+import time
+import sys
+import random
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import Image
 
 
 host_name = '192.168.1.229'    # Change this to your Raspberry Pi IP address
 host_port = 8000
 led_pin = 11
+
+gif_folder_name = "dank_gifs"
+os.chdir(os.getcwd()+"/"+gif_folder_name)
+
+#juicy list comprehension
+img_list = [filename for filename in os.listdir(os.getcwd()) if filename.endswith(".gif")]
+
+options = RGBMatrixOptions()
+options.rows = 64
+options.cols = 64
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+options.gpio_slowdown =4
+options.brightness = 50
+
+matrix = RGBMatrix(options = options)
+
+loop_num = 3
+
+def loop_screen():
+    while(True):
+        random.shuffle(img_list)
+        for gif in img_list:
+            image = Image.open(gif)
+            loop_times = loop_num if image.n_frames < 50 else 1
+            #loop_times = loop_num if image.n_frames < 50 else 1
+            for i in range(0, loop_times):
+                for frame in range(0, image.n_frames):
+                    image.seek(frame)
+                    image_copy = image.convert('RGB').copy().resize((64,64))
+                    matrix.SetImage(image_copy)
+                    time.sleep(0.07)
+    try:
+        print("Press CTRL-C to stop.")
+        while True:
+            time.sleep(100)
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 class MyServer(BaseHTTPRequestHandler):
     """ A special implementation of BaseHTTPRequestHander for reading data from
@@ -61,17 +109,18 @@ class MyServer(BaseHTTPRequestHandler):
         GPIO.setup(led_pin,GPIO.OUT)
 
         if post_data == 'On':
-            GPIO.output(led_pin, GPIO.HIGH)
+            loop_screen()
+            #GPIO.output(led_pin, GPIO.HIGH)
         else:
             GPIO.output(led_pin, GPIO.LOW)
         print("LED is {}".format(post_data))
         self._redirect('/')    # Redirect back to the root url
 
-def setup():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(led_pin, GPIO.OUT)
-    GPIO.output(led_pin, GPIO.HIGH)
-    print('using pin%d'%led_pin)
+# def setup():
+#     GPIO.setmode(GPIO.BOARD)
+#     GPIO.setup(led_pin, GPIO.OUT)
+#     GPIO.output(led_pin, GPIO.HIGH)
+#     print('using pin%d'%led_pin)
 
 
 if __name__ == '__main__':
